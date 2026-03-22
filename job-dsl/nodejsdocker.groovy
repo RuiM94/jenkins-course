21ef4b9f28d12794d2d0450aa413c9512b547c58
RuiM94 <93403720+RuiM94@users.noreply.github.com>
2026-03-22 14:45:58 +0100

    Update nodejsdocker.groovy

--- job-dsl/nodejsdocker.groovy
+++ job-dsl/nodejsdocker.groovy
@@ -1,8 +1,8 @@
 job('NodeJS Docker example') {
     scm {
-        git('https://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        git('https://github.com/RuiM94/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('RuiM94')
+            node / gitConfigEmail('ruimiguelhortasilva@gmail.com')
         }
     }
     triggers {
@@ -14,7 +14,7 @@ job('NodeJS Docker example') {
     }
     steps {
         dockerBuildAndPublish {
-            repositoryName('wardviaene/docker-nodejs-demo')
+            repositoryName('ruimiguel/rui')
             tag('${GIT_REVISION,length=9}')
             registryCredentials('dockerhub')
             forcePull(false)
